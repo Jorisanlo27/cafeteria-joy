bcb5db9b5dbb93fca8f58175adcbe4cf781bf008
Jorge Santana <jorgersantana27@gmail.com>
2023-09-29 02:04:05 -0400

    Campus Model Class - Scaffolded

---
+++ Models/Campus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cafeteria_joy.Models;
+
+public partial class Campus
+{
+    public int CampusId { get; set; }
+
+    public string Descripcion { get; set; } = null!;
+
+    public bool Estado { get; set; }
+
+    public virtual ICollection<Cafeterium> Cafeteria { get; set; } = new List<Cafeterium>();
+}
