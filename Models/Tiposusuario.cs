75e98459fdd50f81a8427261460d9185eccc1666
Yefri Fabian Rodríguez Abreu <y.rodriguez102@unapec.edu.do>
2023-10-30 02:03:26 -0400

    Tiposusuario validations - Added

--- Models/Tiposusuario.cs
+++ Models/Tiposusuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace cafeteria_joy.Models;
 
@@ -7,6 +8,7 @@ public partial class Tiposusuario
 {
     public int TipoUsuarioId { get; set; }
 
+    [RegularExpression(@"^[^@#$*]+$", ErrorMessage = "El campo no puede contener caracteres especiales como @, #, $, *")]
     public string Descripcion { get; set; } = null!;
 
     public bool Estado { get; set; }
