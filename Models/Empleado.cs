51450a6bd31cce08eced0d80efa2bd4063a5d470
Yefri Fabian Rodríguez Abreu <y.rodriguez102@unapec.edu.do>
2023-10-30 02:02:20 -0400

    Empleado validations - Added

--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using cafeteria_joy.Validators;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -8,15 +9,22 @@ public partial class Empleado
 {
     public int EmpleadosId { get; set; }
 
+    [RegularExpression(@"^[^@#$*]+$", ErrorMessage = "El campo no puede contener caracteres especiales como @, #, $, *")]
     public string Nombre { get; set; } = null!;
 
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "The field Cédula must be a string with a length of 11.")]
+    [CedulaValidator(nameof(Usuario))]
+    [Display(Name = "Cédula")]
     public string Cedula { get; set; } = null!;
 
+    [RegularExpression(@"^[^@#$*]+$", ErrorMessage = "El campo no puede contener caracteres especiales como @, #, $, *")]
     public string TandaLabor { get; set; } = null!;
 
+    [Range(0, double.MaxValue, ErrorMessage = "El porcentaje de comisión no puede ser negativo.")]
     public decimal? PorcientoComision { get; set; }
 
     [DataType(DataType.Date)]
+    [Range(typeof(DateTime), "10/1/2023", "12/31/9999", ErrorMessage = "La fecha no puede ser anterior al 1 de octubre de 2023.")]
     public DateTime? FechaIngreso { get; set; }
 
     public bool Estado { get; set; }
