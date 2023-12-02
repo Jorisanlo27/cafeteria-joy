77aba20834e42141d37c1b7c5d41716081c3331d
Jorge Santana <jorgersantana27@gmail.com>
2023-12-01 21:48:01 -0400

    Authorize Controller Routes Depending Role - Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using cafeteria_joy.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace cafeteria_joy.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
