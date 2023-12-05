815107cf31f8c2d049e1d7b07864b13559fc09b6
omar sanchez cuevas <OmR_Work@hotmail.com>
2023-12-04 20:00:29 -0400

    Crud Línea factura

---
+++ Controllers/LineafacturasController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cafeteria_joy.Models;
+
+namespace cafeteria_joy.Controllers
+{
+    public class LineafacturasController : Controller
+    {
+        private readonly JoyContext _context;
+
+        public LineafacturasController(JoyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lineafacturas
+        public async Task<IActionResult> Index()
+        {
+            var joyContext = _context.Lineafacturas.Include(l => l.Articulo).Include(l => l.FacturacionArticulos);
+            return View(await joyContext.ToListAsync());
+        }
+
+        // GET: Lineafacturas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Lineafacturas == null)
+            {
+                return NotFound();
+            }
+
+            var lineafactura = await _context.Lineafacturas
+                .Include(l => l.Articulo)
+                .Include(l => l.FacturacionArticulos)
+                .FirstOrDefaultAsync(m => m.LineaFacturaId == id);
+            if (lineafactura == null)
+            {
+                return NotFound();
+            }
+
+            return View(lineafactura);
+        }
+
+        // GET: Lineafacturas/Create
+        public IActionResult Create()
+        {
+            ViewData["ArticuloId"] = new SelectList(_context.Articulos, "ArticuloId", "Descripcion");
+            ViewData["FacturacionArticulosId"] = new SelectList(_context.Facturacionarticulos, "FacturacionArticulosId", "NoFactura");
+            return View();
+        }
+
+        // POST: Lineafacturas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("LineaFacturaId,FacturacionArticulosId,ArticuloId,UnidadesVendidas,Total")] Lineafactura lineafactura)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lineafactura);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArticuloId"] = new SelectList(_context.Articulos, "ArticuloId", "Descripcion", lineafactura.ArticuloId);
+            ViewData["FacturacionArticulosId"] = new SelectList(_context.Facturacionarticulos, "FacturacionArticulosId", "NoFactura", lineafactura.FacturacionArticulosId);
+            return View(lineafactura);
+        }
+
+        // GET: Lineafacturas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Lineafacturas == null)
+            {
+                return NotFound();
+            }
+
+            var lineafactura = await _context.Lineafacturas.FindAsync(id);
+            if (lineafactura == null)
+            {
+                return NotFound();
+            }
+            ViewData["ArticuloId"] = new SelectList(_context.Articulos, "ArticuloId", "Descripcion", lineafactura.ArticuloId);
+            ViewData["FacturacionArticulosId"] = new SelectList(_context.Facturacionarticulos, "FacturacionArticulosId", "NoFactura", lineafactura.FacturacionArticulosId);
+            return View(lineafactura);
+        }
+
+        // POST: Lineafacturas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("LineaFacturaId,FacturacionArticulosId,ArticuloId,UnidadesVendidas,Total")] Lineafactura lineafactura)
+        {
+            if (id != lineafactura.LineaFacturaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lineafactura);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LineafacturaExists(lineafactura.LineaFacturaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArticuloId"] = new SelectList(_context.Articulos, "ArticuloId", "Descripcion", lineafactura.ArticuloId);
+            ViewData["FacturacionArticulosId"] = new SelectList(_context.Facturacionarticulos, "FacturacionArticulosId", "NoFactura", lineafactura.FacturacionArticulosId);
+            return View(lineafactura);
+        }
+
+        // GET: Lineafacturas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Lineafacturas == null)
+            {
+                return NotFound();
+            }
+
+            var lineafactura = await _context.Lineafacturas
+                .Include(l => l.Articulo)
+                .Include(l => l.FacturacionArticulos)
+                .FirstOrDefaultAsync(m => m.LineaFacturaId == id);
+            if (lineafactura == null)
+            {
+                return NotFound();
+            }
+
+            return View(lineafactura);
+        }
+
+        // POST: Lineafacturas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Lineafacturas == null)
+            {
+                return Problem("Entity set 'JoyContext.Lineafacturas'  is null.");
+            }
+            var lineafactura = await _context.Lineafacturas.FindAsync(id);
+            if (lineafactura != null)
+            {
+                _context.Lineafacturas.Remove(lineafactura);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LineafacturaExists(int id)
+        {
+          return (_context.Lineafacturas?.Any(e => e.LineaFacturaId == id)).GetValueOrDefault();
+        }
+    }
+}
--- Models/Lineafactura.cs
+++ Models/Lineafactura.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace cafeteria_joy.Models;
 
@@ -7,15 +8,19 @@ public partial class Lineafactura
 {
     public int LineaFacturaId { get; set; }
 
+    [Display(Name = "No factura")]
     public int FacturacionArticulosId { get; set; }
-
+    [Display(Name = "Artículo")]
     public int ArticuloId { get; set; }
 
+    [Display(Name = "Unidades vendidas")]
+    [Range(0, int.MaxValue, ErrorMessage = "Las unidades no puedes ser negativas.")]
     public int UnidadesVendidas { get; set; }
 
     public decimal? Total { get; set; }
 
-    public virtual Articulo Articulo { get; set; } = null!;
+    public virtual Articulo? Articulo { get; set; } = null!;
 
-    public virtual Facturacionarticulo FacturacionArticulos { get; set; } = null!;
+    [Display(Name = "No factura")]
+    public virtual Facturacionarticulo? FacturacionArticulos { get; set; } = null!;
 }
